54cc0dc281918c13b55ee84ae49b16565886bc08
Mark Lopez <m@silvenga.com>
2016-09-23 19:38:21 -0500

    Fixed overwriting config file every startup.

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ ! -f /var/log/messages ]; then
+if [ ! -f /sync/syncserver.ini ]; then
     echo "Creating default syncserver.ini."
     cp /usr/local/apache2/syncserver/syncserver.ini.orig /sync/syncserver.ini
 fi
 
 echo "Updating links."
-ln -s /sync/syncserver.ini /usr/local/apache2/syncserver/syncserver.ini
+ln -sf /sync/syncserver.ini /usr/local/apache2/syncserver/syncserver.ini
 mkdir -p /sync/logs
-ln -s /sync/logs /var/log/apache2
+ln -sf /sync/logs /var/log/apache2
 
 echo "Updating permissions."
 chown sync-user:sync-user /usr/local/apache2/syncserver -R
